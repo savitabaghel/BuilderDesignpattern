394436d3d582b5ec290ac0d365203c347b481d71
Savita Baghel <savitabaghel159@gmail.com>
2022-02-16 11:07:58 +0530

    log4j

--- BuilderDesignpattern/src/main/java/com/example/BuilderDesignpattern/BuilderDesignpatternApplication.java
+++ BuilderDesignpattern/src/main/java/com/example/BuilderDesignpattern/BuilderDesignpatternApplication.java
@@ -1,13 +1,16 @@
 package com.example.BuilderDesignpattern;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 
 @SpringBootApplication
 public class BuilderDesignpatternApplication
 {
+
+	private static Logger logger= LogManager.getLogger(BuilderDesignpatternApplication.class);
 	public static void main(String[] args)
 	{
 		Computer comp = new Computer.ComputerBuilder(
@@ -19,7 +22,14 @@ public class BuilderDesignpatternApplication
 				"300 GB", "2 GB","5th").setBluetoothEnabled(true).build();
 
 
-		SpringApplication.run(BuilderDesignpatternApplication.class, args);
+		System.out.println("\n hello !!!\n");
+		logger.info("This is information msg");
+		logger.error("This is error msg");
+		logger.warn("this is warn msg");
+
+		System.out.println("Completed");
+
+
 	}
 
 
